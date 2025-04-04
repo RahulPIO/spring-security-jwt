4b6924a59c8bb6ae333f72f458868e5c5ac25cee
Rahul_PIO <rahul.sharma@programmers.io>
2025-04-04 10:26:26 +0530

    First Commit

---
+++ src/main/java/com/pio/security/jwt/SpringSecurityJwtApplication.java
@@ -0,0 +1,17 @@
+package com.pio.security.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author RahulSharma
+ *
+ */
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+}
